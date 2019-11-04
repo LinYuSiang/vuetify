75828431a95c17083d09143f5e39ff6a19f4c7ae
LinYuSiang <zz020411569@gmail.com>
2019-11-04 18:53:54 +0800

    deploy

---
+++ precache-manifest.a5076547e9ec841130e4aa40496d9f63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/vuetify/robots.txt"
+  },
+  {
+    "revision": "e8cee914adc1b9d35d1e",
+    "url": "/vuetify/js/chunk-vendors.fb892322.js"
+  },
+  {
+    "revision": "86879dfaafd588a1910e",
+    "url": "/vuetify/js/app.b06bef1f.js"
+  },
+  {
+    "revision": "03590e477f2a376585de",
+    "url": "/vuetify/js/about.10b2ccad.js"
+  },
+  {
+    "revision": "e7023b7b3c27d331737ed8592b9b7e1f",
+    "url": "/vuetify/index.html"
+  },
+  {
+    "revision": "e8cee914adc1b9d35d1e",
+    "url": "/vuetify/css/chunk-vendors.685c0563.css"
+  },
+  {
+    "revision": "86879dfaafd588a1910e",
+    "url": "/vuetify/css/app.7b7e7056.css"
+  }
+];
